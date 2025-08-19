15493d8ededfb497b5577645b575939f7321e870
shlomiatia <36788680+shlomiatia@users.noreply.github.com>
2025-08-19 21:12:41 +0300

    cp

--- Entities/DJ/dj.gd
+++ Entities/DJ/dj.gd
@@ -22,9 +22,9 @@ class Track:
 
 var tracks: Array[Track] = [
     Track.new("start.wav", TrackType.ADVANCE_AUTO, preload("res://Music/start.wav")),
-    Track.new("loop1.wav", TrackType.LOOP_UNTIL_POSITION, preload("res://Music/loop1.wav"), 1208.0),
+    Track.new("loop1.wav", TrackType.LOOP_UNTIL_POSITION, preload("res://Music/loop1.wav"), 975.0),
     Track.new("loop1toguitarloop1.wav", TrackType.ADVANCE_AUTO, preload("res://Music/loop1toguitarloop1.wav")),
-    Track.new("guitarloop1.wav", TrackType.LOOP_UNTIL_POSITION, preload("res://Music/guitarloop1.wav"), 2036.0),
+    Track.new("guitarloop1.wav", TrackType.LOOP_UNTIL_POSITION, preload("res://Music/guitarloop1.wav"), 1800.0),
     Track.new("singer1.wav", TrackType.ADVANCE_AUTO, preload("res://Music/singer1.wav")),
     Track.new("singer1tosong1.wav", TrackType.WAIT_FOR_MOUSE_BUTTONS, preload("res://Music/singer1tosong1.wav")),
     Track.new("song1.wav", TrackType.ADVANCE_AUTO, preload("res://Music/song1.wav"))
@@ -55,6 +55,7 @@ func _process(_delta: float) -> void:
                         advance_track()
 
 func _on_finished() -> void:
+    print(player.global_position)
     var current_track = tracks[current_track_index]
     
     match current_track.type:
--- Entities/Player/player.gd
+++ Entities/Player/player.gd
@@ -23,5 +23,3 @@ func _physics_process(delta: float) -> void:
     velocity += get_gravity() * delta
     _current_state._physics_process(delta)
     move_and_slide()
-    if Input.is_action_just_pressed("ui_select"):
-        print(global_position)
