8c38486cbb5b60c48fa5b5409ca389bc855be033
shlomiatia <36788680+shlomiatia@users.noreply.github.com>
2025-08-22 19:12:31 +0300

    cp

--- Entities/Player/player_state_sprint.gd
+++ Entities/Player/player_state_sprint.gd
@@ -1,7 +1,7 @@
 class_name PlayerStateSprint extends PlayerState
 
-var beats = [1.6, 2.5, 3.4, 4.3, 5.2, 5.65, 6.1, 6.55, 7.0, 7.45, 7.9, 8.12, 8.34, 8.56, 8.78, 9.00, 9.22, 9.44, 9.66, 9.88, 10.10, 10.32, 10.54, 10.76, 10.98, 11.20, 11.35, 11.50, 11.65, 11.80, 11.95, 12.10, 12.25, 12.40, 12.55, 12.70, 12.85, 13.00, 13.15, 13.30, 13.45, 13.60, 13.75, 13.90, 14.05, 14.20]
-var beat_circles: Array[Beat] = []
+var beat_times = [1.6, 2.5, 3.4, 4.3, 5.2, 5.65, 6.1, 6.55, 7.0, 7.45, 7.9, 8.12, 8.34, 8.56, 8.78, 9.00, 9.22, 9.44, 9.66, 9.88, 10.10, 10.32, 10.54, 10.76, 10.98, 11.20, 11.35, 11.50, 11.65, 11.80, 11.95, 12.10, 12.25, 12.40, 12.55, 12.70, 12.85, 13.00, 13.15, 13.30, 13.45, 13.60, 13.75, 13.90, 14.05, 14.20]
+var beats: Array[Beat] = []
 var beat_scene: PackedScene = preload("res://Entities/Beat/beat.tscn")
 
 func _init(p_player: Player) -> void:
@@ -12,10 +12,6 @@ func enter() -> void:
     player.animated_sprite_2d.flip_h = true
     schedule_beats()
 
-func exit() -> void:
-    for circle in beat_circles:
-        circle.queue_free()
-    beat_circles.clear()
 
 func _physics_process(_delta: float) -> void:
     player.velocity.x = Constants.player_sprint_speed
@@ -23,38 +19,38 @@ func _physics_process(_delta: float) -> void:
     handle_input()
 
 func schedule_beats() -> void:
-    for beat_time in beats:
-        create_beat_circle(beat_time)
+    for beat_time in beat_times:
+        create_beat(beat_time)
 
-func create_beat_circle(target_time: float) -> void:
-    var circle = beat_scene.instantiate() as Beat
+func create_beat(target_time: float) -> void:
+    var beat = beat_scene.instantiate() as Beat
     
-    circle.position = Vector2(320, -8)
-    circle.target_time = target_time
+    beat.position = Vector2(320, -8)
+    beat.target_time = target_time
     
-    player.get_parent().get_node("CanvasLayer").add_child(circle)
-    beat_circles.append(circle)
+    player.get_parent().get_node("CanvasLayer").add_child(beat)
+    beats.append(beat)
 
 func update_beat_circles() -> void:
     var current_time = player.dj.get_playback_position()
     
-    for circle in beat_circles:
-        var target_time = circle.target_time
+    for beat in beats:
+        var target_time = beat.target_time
         var start_time = target_time - Constants.beat_appear_time
         var end_time = target_time + Constants.beat_appear_time
 
         
         if current_time >= start_time && current_time <= end_time:
             var progress = (current_time - start_time) / Constants.beat_appear_time / 2
-            circle.position.y = lerp(0.0, 368.0, progress)
+            beat.position.y = lerp(0.0, 368.0, progress)
 
 func handle_input() -> void:
     if Input.is_action_just_pressed("dash"):
         var current_time = player.dj.get_playback_position()
         
-        for circle in beat_circles:
-            var target_time = circle.target_time;
+        for beat in beats:
+            var target_time = beat.target_time;
             
             if abs(current_time - target_time) <= Constants.beat_click_threshold:
-                circle.hit()
+                beat.hit()
                 return
