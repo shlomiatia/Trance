0bc2951ebbff2bc71678645adea5f716a49f9fd2
shlomiatia <36788680+shlomiatia@users.noreply.github.com>
2025-08-29 00:26:37 +0300

    cp

--- Entities/Player/player.gd
+++ Entities/Player/player.gd
@@ -2,8 +2,10 @@ class_name Player extends CharacterBody2D
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+@onready var camera_2d: Camera2D = $Camera2D
 
 @export var dj: DJ
+@export var tile_map_layer: TileMapLayer
 
 var _states: Dictionary = {}
 var _current_state: PlayerState
@@ -29,6 +31,13 @@ func change_state(state_type: PlayerStateEnum.Type) -> void:
 func _physics_process(delta: float) -> void:
     if dj.get_current_track() == "song3.wav":
         collision_shape_2d.disabled = true
+    
+    if dj.get_current_track() == "singer2tosong3.wav":
+        var track_position = dj.get_playback_position()
+        var track_length = dj.get_stream().get_length()
+        var progress = track_position / track_length
+        tile_map_layer.modulate.a = lerp(1.0, 0.0, progress)
+    
     var gravity = Vector2(0, 250)
     if dj.get_current_track() == "song2.wav":
         gravity = Vector2(0, -250)
@@ -41,9 +50,9 @@ func _on_track_changed(track_name: String) -> void:
         change_state(PlayerStateEnum.Type.SPRINT)
     elif track_name == "song1.wav" || track_name == "song2.wav":
         #global_position.x = 5600
-        global_position.x = 8600
-        change_state(PlayerStateEnum.Type.FALL)
-        #change_state(PlayerStateEnum.Type.JUMP)
+        # global_position.x = 8600
+        # change_state(PlayerStateEnum.Type.FALL)
+        change_state(PlayerStateEnum.Type.JUMP)
     elif track_name == "song1tosahi.wav":
         global_position = Vector2(6320, 0)
         velocity.y = 125
--- Entities/Player/player_state_sprint.gd
+++ Entities/Player/player_state_sprint.gd
@@ -6,7 +6,13 @@ func _init(p_player: Player) -> void:
 func enter() -> void:
     var track_name = player.dj.tracks[player.dj.current_track_index].file_name
     var track_length = player.dj.stream.get_length()
-    var flat_end_position = 4448 if track_name == "singer1tosong1.wav" else 7440
+    var flat_end_position
+    if track_name == "singer1tosong1.wav":
+        flat_end_position = 4448
+    elif track_name == "sahi2.wav":
+        flat_end_position = 7440
+    elif track_name == "singer2tosong3.wav":
+        flat_end_position = 2944 - 944 * sqrt(2)
     player.animated_sprite_2d.play("run")
     player.animated_sprite_2d.flip_h = true
     var starting_position = player.position.x
